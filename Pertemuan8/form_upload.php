c4931520fd4a9372ddd7825b5ea86cea79ed1e48
Agta Fadjrin Aminullah <fadjrinagta@gmail.com>
2024-04-08 15:22:12 +0700

    Penambahan Soal 1.3

--- Pertemuan8/form_upload.php
+++ Pertemuan8/form_upload.php
@@ -1,14 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <title>File Upload</title> <!--Judul-->
+    <title>File Upload</title>
 </head>
 <body>
-    <!-- Membuat Formulir-->
+    <!-- Pembuatan thumbnail gambar dengan ukuran 200 -->
+    <script>
+        function previewImage(event) {
+            var file = event.target.files[0];
+            var read = new FileReader();
+            read.onload = function(e) {
+                var previewImage = document.getElementById("preview");
+                previewImage.style.display = "block";
+                previewImage.src = e.target.result;
+
+                // Mengatur lebar gambar menjadi 200px dan tinggi disesuaikan secara otomatis
+                previewImage.style.width = "200px";
+                previewImage.style.height = "auto";
+            }
+            read.readAsDataURL(file);
+        }
+    </script>
+    
     <form action="upload.php" method="post" enctype="multipart/form-data">
-        <!--Membuat Inputan-->
-        <input type="file" name="fileToUpload" id="fileToUpload"> <!--tipenya file, dengan nama fileToUpload-->
-        <input type="submit" value="Upload File" name="submit"> <!--tipenya submit, dengan nilai Upload File"-->
+        <input type="file" name="fileToUpload" id="fileToUpload" onchange="previewImage(event)">
+        <img id="preview" src="#" alt="Preview" style="display: none;"> <!-- gambar akan ditampilkan di sini -->
+        <input type="submit" value="Upload File" name="submit">
     </form>
 </body>
 </html>
